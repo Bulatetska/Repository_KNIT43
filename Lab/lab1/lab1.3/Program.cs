9b3abb1abb5e60394966b53b7febdd7750efe3cc
FilnarQ <84853759+FilnarQ@users.noreply.github.com>
2022-09-21 16:04:15 +0300

    lab1.3

--- Lab/lab1/lab1.3/Program.cs
+++ Lab/lab1/lab1.3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 5;
+int b = 2;
+System.Console.WriteLine("a = " + a + ", b = " + b);
+int result = a + b;
+System.Console.WriteLine("Додавання, a + b = )" + result);
+result = a * b;
+System.Console.WriteLine("Множення, a * b = )" + result);
+result = a / b;
+System.Console.WriteLine("Ділення, a / b = )" + result + " a та b - цілі числа, результат - теж");
+double resultDouble = a / b;
+System.Console.WriteLine("Ділення, a / b = )" + resultDouble + " помилка...");
+double aDouble= 5;
+resultDouble = aDouble / b;
+System.Console.WriteLine("Ділення, a / b = )" + resultDouble);
+System.Console.ReadLine();
