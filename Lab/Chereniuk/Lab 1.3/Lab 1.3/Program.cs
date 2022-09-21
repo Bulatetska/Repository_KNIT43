d45a0f9eb2f32227b53182982c1497ab97d85fd0
Archerusoff <114077293+Archerusoff@users.noreply.github.com>
2022-09-21 16:06:54 +0300

    lab

---
+++ Lab/Chereniuk/Lab 1.3/Lab 1.3/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 5;
+int b = 2;
+System.Console.WriteLine("a =
++
+a +
+b = " + b);
+int result = a + b;
+System.Console.WriteLine("Додавання, a + b =
+" + result);
+result = a * b;
+System.Console.WriteLine("Множення, a * b =
+" + result);
+result = a / b;
+System.Console.WriteLine("Ділення, а / b = " + result + " а та b - цілі числа, результат - теж");
+double resultDouble = a / b;
+System.Console.WriteLine("Ділення, a / b = " + resultDouble + " помилка...
+");
+double aDouble = 5;
+resultDouble = aDouble / b;
+System.Console.WriteLine("Ділення, а / b =
+" + resultDouble);
+System.Console.ReadLine();
+
