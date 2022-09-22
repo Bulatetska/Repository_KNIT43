6ba9830755af75891b957fb4a747b5f19bdc0339
kuinke <112954099+lytvynchukkk@users.noreply.github.com>
2022-09-22 15:28:58 +0300

    5

---
+++ Lab/lytvynchuk/lab1/lab1.2/lab1.2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool boolVariable = true;
+            if(boolVariable)
+            {
+                System.Console.WriteLine("boolVariable = true;");
+            }
+            else
+            {
+                System.Console.WriteLine("boolVariable = false;");
+            }
+            System.Console.WriteLine();
+
+            boolVariable = false;
+            if (boolVariable)
+            {
+                System.Console.WriteLine("boolVariable = true;");
+            }
+            else
+            {
+                System.Console.WriteLine("boolVariable = false;");
+            }
+
+            boolVariable = 2 < 4;
+            if (boolVariable)
+            {
+                System.Console.WriteLine("boolVariable = 2 < 4; true;");
+            }
+            else
+            {
+                System.Console.WriteLine("boolVariable = 2 < 4; false;");
+            }
+            if(10!=100)
+            {
+                System.Console.WriteLine("10!=100! diferent numbers!");
+            }
+            System.Console.WriteLine();
+            System.Console.ReadLine();
+
+
+        }
+    }
+}
