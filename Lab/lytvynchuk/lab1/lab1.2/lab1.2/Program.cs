11c7717efbecef71c43d743ada43c23dc8157904
kuinke <112954099+lytvynchukkk@users.noreply.github.com>
2022-09-22 15:34:54 +0300

    6

--- Lab/lytvynchuk/lab1/lab1.2/lab1.2/Program.cs
+++ Lab/lytvynchuk/lab1/lab1.2/lab1.2/Program.cs
@@ -48,6 +48,41 @@ namespace lab1._2
             System.Console.ReadLine();
 
 
+
+
+      
+  
+            Console.Write("a = ");
+            var a = double.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            var b = double.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            var c = double.Parse(Console.ReadLine());
+
+            double x1, x2;
+            var discriminant = Math.Pow(b, 2) - 4 * a * c;
+            if (discriminant < 0)
+            {
+                Console.WriteLine("rozv9zkiv nema");
+            }
+            else
+            {
+                if (discriminant == 0) 
+                {
+                    x1 = -b / (2 * a);
+                    x2 = x1;
+                }
+                else 
+                {
+                    x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                }
+            
+                Console.WriteLine($"x1 = {x1}; x2 = {x2}");
+            }
+
+            Console.ReadKey(true);
+
         }
     }
 }
