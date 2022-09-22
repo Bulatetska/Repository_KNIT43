95082428b1189793d14a1943a7b27552712beb2b
FilnarQ <84853759+FilnarQ@users.noreply.github.com>
2022-09-22 15:45:14 +0300

    3.2

---
+++ Lab/Lab3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿static string fullPIB(string surname, string name, string patronymic)
+{
+    string fio = surname + " " + name + " " + patronymic;
+    return fio;
+}
+static string shortPIB(string surname, string name, string patronymic)
+{
+    string fio = surname + " " + name.Substring(0, 1) + ". " + patronymic.Substring(0, 1) + ".";
+    return fio;
+}
+
+
+string[,] FIO = {
+  {"Adobe", "Samsung", "Ashanovych"},
+  {"Marchuk", "Dmytro", "Grygorovych"},
+  {"Patsiuk", "Alina", "Olekseevna"}
+};
+
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(fullPIB(FIO[i, 0], FIO[i, 1], FIO[i, 2]));
+    Console.WriteLine(shortPIB(FIO[i, 0], FIO[i, 1], FIO[i, 2]));
+}
+Console.ReadLine();
