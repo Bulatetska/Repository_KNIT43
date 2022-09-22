5502fa3f5023a5f273bf0a1cfc994a12b6329274
K-ossi <79997065+K-ossi@users.noreply.github.com>
2022-09-22 15:45:45 +0300

    rrr

--- Lab/Handziuk/Lab5/Lab5.1/Lab5.1/Program.cs
+++ Lab/Handziuk/Lab5/Lab5.1/Lab5.1/Program.cs
@@ -10,6 +10,21 @@ namespace Lab5._1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Input surname ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Input A ");  //99,35
+            float a = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Input I ");  //72
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input C ");  //1995
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input L ");   //true
+            bool l = Convert.ToBoolean(Console.ReadLine());
+
+            System.Console.WriteLine("Результати формування    \n name = {0,6}, l = {1,4}",name,l);
+            System.Console.WriteLine("a = {0,4}, c = {0,10:f5}, i = {2,20:e8},", a, c, i);
+            System.Console.WriteLine(" ");
+            Console.ReadLine();
         }
     }
 }
