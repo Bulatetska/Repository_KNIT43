c2cc378787e5a67ca083a7895f7d40a314734184
FilnarQ <84853759+FilnarQ@users.noreply.github.com>
2022-09-22 15:25:48 +0300

    2.1

---
+++ Lab/Lab2.1/Program.cs
@@ -0,0 +1,40 @@
+﻿bool boolVariable = true;
+
+if (boolVariable)
+{
+    System.Console.WriteLine("boolVariavle = true");
+}
+else
+{
+    System.Console.WriteLine("boolVariavle = false");
+}
+System.Console.WriteLine();
+
+boolVariable = false;
+if (boolVariable)
+{
+    System.Console.WriteLine("boolVariavle = true");
+}
+else
+{
+    System.Console.WriteLine("boolVariavle = false");
+}
+System.Console.WriteLine();
+
+boolVariable = 2 < 4;
+if (boolVariable)
+{
+    System.Console.WriteLine("boolVariavle = 2 < 4 = true");
+}
+else
+{
+    System.Console.WriteLine("boolVariavle = 2 < 4 = false");
+}
+
+if (10 != 100)
+{
+    System.Console.WriteLine("10 != 100! Різні числа!");
+}
+
+System.Console.WriteLine();
+System.Console.ReadLine();
