32a699ebf4bb525e0b76627ce14155b54997e236
FilnarQ <84853759+FilnarQ@users.noreply.github.com>
2022-09-22 15:58:34 +0300

    4.1

---
+++ Lab/Lab4.1/Program.cs
@@ -0,0 +1,29 @@
+﻿{
+    Random rand = new Random();
+    int[] value = new int[30];
+    for (int i = 0; i < 30; i++)
+    {
+        value[i] = rand.Next(0, 20);
+    }
+    int max = 0, maxCount = 0, maxPos = 0;
+    for (int i = 0; i < 30; i++)
+    {
+        if (value[i] > max)
+        {
+            max = value[i];
+            maxPos = i;
+        }
+    }
+    foreach (int i in value)
+    {
+        Console.Write(i + ",");
+        if (i == max)
+        {
+            maxCount++;
+        }
+    }
+
+
+    Console.WriteLine("\nMax - " + max + ", " + maxCount + " раз.");
+    Console.WriteLine("Позиція max - " + maxPos);
+}
