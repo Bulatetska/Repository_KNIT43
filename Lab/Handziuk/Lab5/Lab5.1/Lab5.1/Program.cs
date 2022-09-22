e2b672879ccc23abd0aaff363c8569b0ba07ac5c
K-ossi <79997065+K-ossi@users.noreply.github.com>
2022-09-22 15:23:11 +0300

    1

--- Lab/Handziuk/Lab5/Lab5.1/Lab5.1/Program.cs
+++ Lab/Handziuk/Lab5/Lab5.1/Lab5.1/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab
+namespace Lab5._1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
         }
     }
 }
