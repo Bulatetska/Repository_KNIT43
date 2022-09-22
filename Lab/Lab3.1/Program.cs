2be8b6f9cb5e52436b606d4421af888d263d179d
FilnarQ <84853759+FilnarQ@users.noreply.github.com>
2022-09-22 15:41:41 +0300

    3.1

---
+++ Lab/Lab3.1/Program.cs
@@ -0,0 +1,22 @@
+﻿{
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            Console.WriteLine(i + " кратне 3 і 5");
+        }
+        else if (i % 3 == 0)
+        {
+            Console.WriteLine(i + " кратне 3");
+        }
+        else if (i % 5 == 0)
+        {
+            Console.WriteLine(i + " кратне 5");
+        }
+        else
+        {
+            Console.WriteLine(i);
+        }
+    }
+    Console.ReadLine();
+}
