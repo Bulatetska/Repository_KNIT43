c8664915d8eed6d3cd8ef5ddbc36ebb73457d481
K-ossi <katerina.gandziuk.10@gmail.com>
2022-09-23 15:08:12 +0300

    111

---
+++ Lab/Handziuk/Lab2/Lab2.3/Lab2.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            var a = double.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            var b = double.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            var c = double.Parse(Console.ReadLine());
+
+            double s, p, d;
+
+            p = (a + b + c) / 2;
+            d = p * (p - a) * (p - b) * (p - c);
+            if (d < 0) { Console.WriteLine("Трикутника не існує"); }
+            else
+            {
+                s = (double)Math.Sqrt(d);
+                Console.WriteLine("Площа трикутника - " + s);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
