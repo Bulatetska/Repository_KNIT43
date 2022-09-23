86305757aea18590d373e9402f11304e465c5f25
K-ossi <katerina.gandziuk.10@gmail.com>
2022-09-23 14:55:00 +0300

    1.1

---
+++ Lab/Handziuk/Lab1/Lab1.1/Lab1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Console.ReadLine();
+        }
+    }
+}
