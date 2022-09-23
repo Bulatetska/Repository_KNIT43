a18a160af8ad0deb11419bac8dc95d640abce2b3
K-ossi <katerina.gandziuk.10@gmail.com>
2022-09-23 15:17:16 +0300

    4.1

---
+++ Lab/Handziuk/Lab4/Lab4.1/Lab4.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+
+            int i, k = 0;
+            for (i = 0; i < 10; i++)
+            {
+                arr[i] = i;
+                Console.WriteLine(arr[i]);
+            }
+
+
+            int max = arr[0];
+            int n = 1;
+            for (i = 1; i < 10; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+                n = i;
+            }
+            Console.WriteLine("MAX = " + max);
+            Console.WriteLine("Position = " + n);
+
+            for (i = 1; i < 10; i++)
+            {
+                if (max == arr[i])
+                    k++;
+            }
+            Console.WriteLine("K = " + k);
+            Console.ReadLine();
+        }
+    }
+}
