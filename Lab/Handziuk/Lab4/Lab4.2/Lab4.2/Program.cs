bc5bf56b7a9bd8830b8fbcd6de568c8753607de7
K-ossi <katerina.gandziuk.10@gmail.com>
2022-09-23 15:18:48 +0300

    4.2

---
+++ Lab/Handziuk/Lab4/Lab4.2/Lab4.2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Lab4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] D = new int[5, 5];
+            int max = D[0, 0];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    D[i, j] = i + j;
+
+                    Console.Write(D[i, j]);
+                }
+                Console.WriteLine();
+
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+
+                    if (D[i, j] > max)
+                        max = D[i, j];
+
+                }
+                Console.WriteLine("Максимальний елемент " + max);
+
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+
+                    if (D[i, j] == 0)
+                    {
+                        D[i, j] = 0;
+                    }
+                    Console.Write(D[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
