419099f2b6d0ca0f2181c244a9f76cfa0988a68d
K-ossi <katerina.gandziuk.10@gmail.com>
2022-09-23 15:01:06 +0300

    2.1

---
+++ Lab/Handziuk/Lab2/Lab2.1/Lab2.1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lab2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool boolVariable = true;
+
+            if (boolVariable)
+            {
+                System.Console.WriteLine("boolVariable = true; ---Iстина!");
+            }
+            else
+            {
+                System.Console.WriteLine("boolVariable = false; ---Хибно!");
+            }
+            System.Console.WriteLine();
+
+            boolVariable = false;
+            if (boolVariable)
+            {
+                System.Console.WriteLine("boolVariable = false; Iстина!");
+            }
+            else
+            {
+                System.Console.WriteLine("boolVariable = false; Хибно!");
+            }
+
+            boolVariable = 2 < 4;
+
+            if (boolVariable)
+            {
+                System.Console.WriteLine("boolVariable = 2 < 4;  Iстина!");
+            }
+            else
+            {
+                System.Console.WriteLine("boolVariable = 2 < 4; Хибно!");
+            }
+
+            if (10 != 100)
+            {
+                System.Console.WriteLine("10 != 100! Рiзнi числа!");
+            }
+            System.Console.WriteLine();
+            System.Console.ReadLine();
+        }
+    }
+}
