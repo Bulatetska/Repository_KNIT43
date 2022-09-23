dd0203117b83b11e6e989e8c150c88332fff7c6c
K-ossi <katerina.gandziuk.10@gmail.com>
2022-09-23 15:21:01 +0300

    4.6

---
+++ Lab/Handziuk/Lab4/Lab4.6/Lab4.6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab4._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float[] a = new float[10];
+            Random rand = new Random();
+            float sum = 0;
+            int i;
+            for (i = 0; i < 10; i++)
+            {
+                a[i] = rand.Next(10);
+                Console.Write(a[i]);
+            }
+
+            i = 0;
+            while (i < 10)
+            {
+                sum += a[i];
+                i++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Сума: " + sum);
+
+            Console.ReadLine();
+        }
+    }
+}
