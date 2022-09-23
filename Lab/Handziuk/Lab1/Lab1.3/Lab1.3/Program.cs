54701d0ec3489e9df370360ac94b9b15cea622ce
K-ossi <katerina.gandziuk.10@gmail.com>
2022-09-23 14:58:24 +0300

    1.3

---
+++ Lab/Handziuk/Lab1/Lab1.3/Lab1.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 2;
+            System.Console.WriteLine("a= " + a + ",b=" + b);
+            int result = a + b;
+            System.Console.WriteLine("Додавання, a + b = " + result);
+            result = a * b;
+            System.Console.WriteLine("Множення, a * b = " + result);
+            result = a / b;
+            System.Console.WriteLine("Дiлення, a / b = " + result + " a та b цiлi числа, результат - теж");
+            double resultDouble = a / b;
+            System.Console.WriteLine("Дiлення, a / b = " + resultDouble + " помилка...");
+            double aDouble = 5;
+            resultDouble = aDouble / b;
+            System.Console.WriteLine("Дiлення, a / b " + resultDouble);
+            System.Console.ReadLine();
+        }
+    }
+}
