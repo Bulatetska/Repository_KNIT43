4e2c6db73c1b87472c533b67faa81b3ffc70df31
Zubko Karina <karinazubko2017@gmail.com>
2022-10-05 13:26:17 +0300

    lab5_1

---
+++ lab5_1.cs
@@ -0,0 +1,30 @@
+
+
+
+
+using System;
+	class KarinaZubkolab5_1{
+		//мій варіант - 13
+		public static void Main()
+		{
+			int A;  
+			float C;   
+			double I;  
+			bool L;  
+			string N_surname; 
+			Console.WriteLine("Введіть призвище: "); 
+			N_surname = Console.ReadLine(); 
+			Console.WriteLine("Введіть I");
+			A = Convert.ToInt32(Console.ReadLine()); 
+			Console.WriteLine("Введіть C");
+			C = Convert.ToSingle(Console.ReadLine()); 
+			Console.WriteLine("Введіть A");
+			I = Convert.ToDouble(Console.ReadLine()); 
+			Console.WriteLine("Введіть L");
+			L = Convert.ToBoolean(Console.ReadLine());
+			System.Console.WriteLine(" Результат: \n Призвище = {0, 6}, L = {1, 4}", N_surname,L);
+			System.Console.WriteLine("I ={0, 4}, C = {1,10:f5}, A = {2,20:e8}", A,C,I);
+			System.Console.WriteLine(" "); 
+			System.Console.ReadLine();
+		}
+}
