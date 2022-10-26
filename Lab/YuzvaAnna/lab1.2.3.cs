fbce5bae8eaf1aeda62a8f64acd32e8a92c15d6f
AnnaYuzva <annayzva@gmail.com>
2022-10-26 16:31:56 +0300

    Anna

---
+++ Lab/YuzvaAnna/lab1.2.3.cs
@@ -0,0 +1,17 @@
+
+using System;
+{
+	double[] triangle = {12, 5, 13};
+
+	Array.Sort(triangle);
+	Console.WriteLine("Triangle: " + triangle[0] + ", " + triangle[1] + ", " + triangle[2] + ".");
+	if (triangle[0] + triangle[1] > triangle[2]) {
+		double p = (triangle[0] + triangle[1] + triangle[2])/2;
+		double S = Math.Sqrt(p * (p - triangle[0]) * (p - triangle[1]) * (p - triangle[2]));
+		Console.WriteLine("S = " + S + ".");
+	}
+	else {
+		Console.WriteLine("Triangle doesn't exist.");
+	}
+	Console.ReadLine();
+}
