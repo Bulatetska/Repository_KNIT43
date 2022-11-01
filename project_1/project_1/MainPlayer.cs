d6064b4c446d51fdace688c7e1adb2bc26e14f8a
VitalKn <WJknysh1@gmail.com>
2022-11-01 20:22:39 +0200

    Add files via upload

---
+++ project_1/project_1/MainPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_1
+{
+    abstract class Participant
+    {
+        public int wins { get; set; }
+        float _winRate;
+        protected RockPaperScissors selection;
+
+        protected float winRate
+        {
+            get
+            {
+                return _winRate;
+            }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new Exception("value cannot be less than 0 or greater than 100");
+                }
+                _winRate = value;
+            }
+        }
+        public void PrintWinRate()
+        {
+            this.winRate = ((float)wins / Game.Info.GamesPlayed) * 100;
+            string winRate = "win rate: " + this.winRate.ToString() + "%";
+            Console.WriteLine(winRate.PadLeft(50));
+        }
+
+        public abstract RockPaperScissors Select();
+    }
+
+}
---
+++ project_1/project_1/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_1
+{
+    class Player : Participant
+    {
+        public override RockPaperScissors Select()
+        {
+            bool isValid;
+            string input;
+
+            do
+            {
+                Console.Write("Please enter a valid selection: ");
+                input = Console.ReadLine().Trim();
+                isValid = Enum.TryParse<RockPaperScissors>(input, true, out selection);
+            } while (!isValid);
+
+            return selection;
+        }
+    }
+}
