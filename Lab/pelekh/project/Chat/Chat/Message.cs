23c53d1782333ae3ad0a32e57aef27f1f236d1aa
Herman Pelekh <herka03@gmail.com>
2022-11-09 15:12:50 +0200

    _

---
+++ Lab/pelekh/project/Chat/Chat/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat
+{
+    public class Message
+    {
+        public int id;
+        public string author;
+        public string text;
+        public Message(int id, string author, string text)
+        {
+            this.id = id;
+            this.author = author;
+            this.text = text;
+        }
+    }
+}
---
+++ Lab/pelekh/project/Chat/Chat/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat
+{
+    class User
+    {
+        public int id;
+        public string username;
+        public int[] convs = new int[10];
+        private int convsCount = 0;
+        public User(int id, string username)
+        {
+            this.id = id;
+            this.username = username;
+            for(int i=0;i<10;i++)
+            {
+                this.convs[i] = -1;
+            }
+        }
+        public void invited(int id)
+        {
+            this.convs[convsCount++] = id;
+        }
+    }
+}
