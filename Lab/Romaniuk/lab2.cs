e2c0d4bec6c2c5b8bdf52f72fee8b8ba7c3fa573
OleksandrRomaniuk <sasaromanuk230@gmail.com>
2022-11-19 21:16:36 +0200

    add 1,2,3 lab

---
+++ Lab/Romaniuk/lab2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 2;
+            b = 3;
+            c = 5;
+            double D;
+            D = (b * b) - (4 * a * c);
+            if (D > 0)
+            {
+                double x1, x2;
+                x1 = (-b - Math.Sqrt(D)) / (2 * a);
+                x2 = (-b + Math.Sqrt(D)) / (2 * a);
+                Console.WriteLine("X1 = " + x1);
+                Console.WriteLine("X2 = " + x2);
+            }
+            else if (D < 0)
+            {
+                Console.WriteLine("X<0");
+            }
+            else {
+                double x0;
+                x0 = -b / (2 * a);
+                Console.WriteLine("X = " + x0);
+            }
+
+            Console.ReadLine();
+
+            double a, b, c;
+            A = 2;
+            B = 3;
+            C = 4;
+            if (a + b <= c || a + c <= b || b + c <= a)
+            {
+                Console.WriteLine("Not exist");
+            }
+            else {
+                double p;
+                p = (a + b + c) / 2;
+                double S;
+                S = Math.Sqrt(p * (p - a) * (p - b) * (p - b));
+                Console.WriteLine("Area: " + S);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab/Romaniuk/lab3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                n = i;
+                if (n % 3 == 0 && n % 5 == 0)
+                {
+                    Console.WriteLine("3 and 5");
+                }
+                else if (n % 3 == 0)
+                {
+                    Console.WriteLine("of 3");
+                }
+                else if (n % 5 == 0)
+                {
+                    Console.WriteLine("5");
+                }
+                else { Console.WriteLine(n); }
+            }
+            Console.ReadLine();
+
+            CreateFip("Romaniuk", "Oleksandr", "Mukolayovuch");
+
+            Console.ReadLine();
+        }
+        public static void CreateFip(string surname, string name, string pobatkovi) {
+            string fip = surname + " " + name + " " + pobatkovi;
+            Console.WriteLine(fip);
+        }
+    }
+}
