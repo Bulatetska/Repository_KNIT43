0a6d0ff9912a19ab578d06549dd9315e8c29e3d8
Yarmolsky <56644121+YarmolskyProg@users.noreply.github.com>
2022-11-28 01:49:15 +0200

    Add files via upload

---
+++ Lab/Lab4.2/Lab4.2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] D = new int[5,5];
+            int max = D[0, 0];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    D[i,j] = i + j;
+                    
+                    Console.Write(D[i, j]);
+                }
+                Console.WriteLine();
+              
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    
+                if (D[i, j] > max)
+                    max = D[i, j] ;
+                   
+                }
+                Console.WriteLine("Максимальний елемент " + max);
+
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+
+                    if (D[i,j] = 0)
+                    {
+                        D[i, j] = 0;
+                    }
+                    Console.Write(D[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab/Lab4.3/Lab4.3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            float[] a = new float[10];
+            Random rand = new Random();
+            int numb = 0, n = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                a[i] = rand.Next(10);
+            Console.Write(a[i]);
+            }
+            Console.WriteLine();
+            
+            for (int i = 0; i < 9; i++)
+            {
+                if (a[i]>0 && a[i+1]>0) {
+                    numb++;
+                }
+              
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (a[i] == 0 && a[i + 1] == 0)
+                {
+                    n++;
+                }
+            }
+            Console.WriteLine("Додатнi числа: " + numb);
+            Console.WriteLine("Нулi: " + n);
+
+            Console.ReadLine();
+
+        }
+    }
+}
