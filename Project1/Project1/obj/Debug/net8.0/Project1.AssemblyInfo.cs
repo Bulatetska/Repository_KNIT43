e5e2624d47ce9935eda1f95f292ca9af8ea86660
Kolya <113422054+kolyarasik@users.noreply.github.com>
2023-11-22 16:20:19 +0200

    s

--- Project1/Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ Project1/Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe95137784b2a76f06ccd35b3c218d42d8fb42ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
