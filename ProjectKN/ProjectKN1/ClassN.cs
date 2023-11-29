6544c9c3f6ebfb09c162ca609ad4967cb8e06743
omestya <113118725+Omestya@users.noreply.github.com>
2023-11-29 16:26:54 +0200

    com

---
+++ ProjectKN/ProjectKN1/ClassN.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectKN1
+{
+    internal class ClassN
+    {
+        public int Add(int a, int b)
+        { return a + b; }
+    }
+}
