6263f7edc4b5cc9709ed561ada1b73a6200c6c2b
Kolya <113422054+kolyarasik@users.noreply.github.com>
2023-12-13 17:21:59 +0200

    aaa

---
+++ ProjectRK/ProjectRK/Song.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectON
+{
+    public class Song
+    {
+        public string Title { get; set; }   
+        public string Artist { get; set; }
+        public string FilePath { get; set; }
+
+    }
+}
