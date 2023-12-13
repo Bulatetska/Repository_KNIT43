b2ee6930dc9256a7a60353d6ee2a43f680089396
Omestya <113118725+Omestya@users.noreply.github.com>
2023-12-13 17:52:46 +0200

    a

---
+++ 222/ConsoleApp1/Song.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Song
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string FilePath { get; set; }
+    }
+}
