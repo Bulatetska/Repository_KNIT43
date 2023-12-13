b6cce5b1ce5e446e670d4a868e78fa420a0e69ac
Kolya <113422054+kolyarasik@users.noreply.github.com>
2023-12-13 17:12:56 +0200

    Create MusicLibrary.cs

---
+++ ProjectRK/ProjectRK/ProjectRK2/MusicLibrary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectRK2
+{
+    public class MusicLibrary
+    {
+        public List <Song> AllSongs { get; set; } = new List<Song>();
+        public List<Playlist> Playlists { get; set; } = new List<Playlist>();
+
+        public void AddSongToLibrary(Song song)
+        {
+            AllSongs.Add(song);
+        }
+
+        public void RemoveSongFromLibrary(Song song)
+        {
+            AllSongs.Remove(song);
+            // Також слід видалити пісню з усіх плейлистів, де вона може бути
+            foreach (var playlist in Playlists)
+            {
+                playlist.Songs.Remove(song);
+            }
+        }
+
+        public void CreatePlaylist(string playlistName)
+        {
+            Playlist newPlaylist = new Playlist { Name = playlistName };
+            Playlists.Add(newPlaylist);
+        }
+
+        public void RemovePlaylist(Playlist playlist)
+        {
+            Playlists.Remove(playlist);
+        }
+
+        // Додайте інші методи за необхідності, наприклад, отримання інформації про плейлист або пісню
+    }
+}
