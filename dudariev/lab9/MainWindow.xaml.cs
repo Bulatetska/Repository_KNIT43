9d177234b5662ccb26b841464b2ca9ec0e61f004
vcenspxf <231761360+vcenspxf@users.noreply.github.com>
2025-10-17 02:51:36 +0300

    lab9

---
+++ dudariev/lab9/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace lab9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MainWindowViewModel viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = viewModel = new MainWindowViewModel();
+        }
+
+        private void Submit(object sender, RoutedEventArgs e)
+        {
+            string name = viewModel.Name.Trim();
+            viewModel.Message = name != "" ? $"Привіт, {name}!" : "";
+        }
+    }
+}
---
+++ dudariev/lab9/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace lab9
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string name = "";
+        private string message = "";
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Message
+        {
+            get => message;
+            set
+            {
+                message = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
