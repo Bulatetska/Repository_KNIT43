723490aa4c8465699981f992fa977b33e1234051
vcenspxf <231761360+vcenspxf@users.noreply.github.com>
2025-10-17 08:43:17 +0300

    lab11

---
+++ dudariev/lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ dudariev/lab11/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "lab11")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "lab11.Pages")]
