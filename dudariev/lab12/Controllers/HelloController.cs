f27d13e53cf9fa310cf280b269bc0027519439ec
vcenspxf <231761360+vcenspxf@users.noreply.github.com>
2025-10-17 10:30:01 +0300

    lab12

---
+++ dudariev/lab12/Controllers/HelloController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lab12.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return Content("Вітаю! Це ваш перший контролер у ASP.NET Core MVC.");
+            return View();
+        }
+    }
+}
