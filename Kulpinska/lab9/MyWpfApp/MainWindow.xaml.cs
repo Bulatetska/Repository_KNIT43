0c8f54d3b0641bac86452eaa752fb08a3a708ea8
Snizhana <snizanakulpinska@gmail.com>
2025-10-24 19:11:19 +0300

    лабораторна 9 (простий графічний інтерфейс)

---
+++ Kulpinska/lab9/MyWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace MyWpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameInput.Text;
+            GreetingText.Text = $"Привіт, {name}!";
+        }
+    }
+}
