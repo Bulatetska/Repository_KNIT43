1afa5d44167d3b9493b1bcc8ede1ca21a4121756
oksanapavluk05 <oksanapavluk178@gmail.com>
2025-10-30 17:10:46 +0100

    add lab 10

---
+++ Pavliuk/Lab10/Lab10/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Lab10
+{
+    public class Book : INotifyPropertyChanged
+    {
+        private string title;
+        private string author;
+        private int year;
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        public string Author
+        {
+            get { return author; }
+            set
+            {
+                author = value;
+                OnPropertyChanged("Author");
+            }
+        }
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                year = value;
+                OnPropertyChanged("Year");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
---
+++ Pavliuk/Lab10/Lab10/BookViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace Lab10
+{
+    public class BookViewModel : INotifyPropertyChanged
+    {
+        private Book selectedBook;
+        public ObservableCollection<Book> Books { get; set; }
+        public Book SelectedBook
+        {
+            get { return selectedBook; }
+            set
+            {
+                selectedBook = value;
+                OnPropertyChanged("SelectedBook");
+            }
+        }
+        public BookViewModel()
+        {
+            Books = new ObservableCollection<Book> { 
+                new Book {Title = "Alice in Wonderland", Author="Luis Carroll", Year = 1865},
+                new Book {Title = "Golden Boys", Author="Phil Stamper", Year = 2019},
+                new Book {Title = "Pollianna", Author="a", Year = 1850},
+            };
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                    (addCommand = new RelayCommand(obj =>
+                    {
+                        Book book = new Book();
+                        Books.Insert(0, book);
+                        SelectedBook = book;
+                    }));
+            }
+        }
+
+        private RelayCommand removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ??
+                    (removeCommand = new RelayCommand(obj =>
+                    {
+                        Book book = obj as Book;
+                        if (book != null) { 
+                            Books.Remove(book);
+                        }
+                    },
+                    (obj)=> Books.Count>0));
+            }
+        }
+    }
+}
