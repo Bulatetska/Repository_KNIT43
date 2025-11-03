3fa872f54b7a5780ea84c45bb4736bd3337d0a27
andrij-dudko <andrijdudko905@gmail.com>
2025-11-03 13:39:03 +0200

    lab9

---
+++ dudko/Лаб-9/HelloWpfApp/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HelloWpfApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ dudko/Лаб-9/HelloWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace HelloWpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameInput.Text;
+
+            if (!string.IsNullOrWhiteSpace(name))
+                GreetingText.Text = $"Привіт, {name}!";
+            else
+                GreetingText.Text = "Будь ласка, введіть ім'я.";
+        }
+    }
+}
