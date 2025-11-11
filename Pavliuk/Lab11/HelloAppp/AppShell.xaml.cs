416016dafccff2108f1eb4d63b562fe9108db271
oksanapavluk05 <oksanapavluk178@gmail.com>
2025-11-11 12:21:53 +0100

    add lab11
    додано але не змогла підключити до фізичного пристрою андроїд

---
+++ Pavliuk/Lab11/HelloAppp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloAppp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Pavliuk/Lab11/HelloAppp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "HelloAppp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "HelloAppp.Pages")]
