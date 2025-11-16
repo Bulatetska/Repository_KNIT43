76a0a1c889d4d31bd1b539ec9fde34ad9b7f50a9
Snizhana <snizanakulpinska@gmail.com>
2025-11-16 19:21:53 +0200

    лабораторна 11 ( MAUI)

---
+++ Kulpinska/lab11/MauiApp1/MauiApp1/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiApp1")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiApp1.Pages")]
