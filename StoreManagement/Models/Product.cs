1b0c9458be5c8bdee92dc960e5e9390132629bdd
Snizhana <snizanakulpinska@gmail.com>
2025-12-08 13:35:05 +0200

    клас Продукт

---
+++ StoreManagement/Models/Product.cs
@@ -0,0 +1,23 @@
+namespace StoreManagement.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product() { }
+
+        public Product(string name, decimal price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} — {Price} грн, кількість: {Quantity}";
+        }
+    }
+}
