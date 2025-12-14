91096ae7630a3390f634bbb704cde3198b069ce0
IronUmbrellal <irinafutruk932@gmail.com>
2025-12-14 19:02:47 +0200

    створено список фільмів,реалізовано клік по них, перероблено щоб виглядало красивіше

--- Cinema/Cinema_wpf/View/MainWindow.xaml.cs
+++ Cinema/Cinema_wpf/View/MainWindow.xaml.cs
@@ -1,16 +1,11 @@
 ﻿using System.Windows;
 using Cinema_wpf.ViewModel; 
-
-namespace Cinema_wpf.View
-{
+namespace Cinema_wpf.View;
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-
-            
             DataContext = new MainViewModel();
         }
     }
-}
