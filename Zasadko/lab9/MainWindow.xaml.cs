2da271171467b880d23dd2cbc7e2aabe87c4a8fa
Ostap Zasadko <ost.zasadko@gmail.com>
2025-12-21 18:18:18 +0200

    lab9

---
+++ Zasadko/lab9/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace HelloApp
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _userName;
+        private string _greeting;
+
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                _userName = value;
+                OnPropertyChanged(nameof(UserName));
+            }
+        }
+
+        public string Greeting
+        {
+            get => _greeting;
+            set
+            {
+                _greeting = value;
+                OnPropertyChanged(nameof(Greeting));
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            Greeting = $"Привіт, {UserName}!";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
