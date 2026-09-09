6db7e18c71dcbb0efa9b1f94915bc97a832ccb67
Romanchuk <93906215+Toxpar@users.noreply.github.com>
2026-09-09 14:23:30 +0300

    Add HelloWorld.cs with basic Hello World program

---
+++ Romanchuk/HelloWorld.cs
@@ -0,0 +1,16 @@
+using System; 
+
+namespace HelloWorldApp { 
+    
+    class Geeks { 
+        
+        static void Main(string[] args) { 
+            
+            // statement printing Hello World! 
+            Console.WriteLine("Hello World!"); 
+            
+            // To prevent the screen from running and closing quickly 
+            Console.ReadKey(); 
+        } 
+    } 
+} 
