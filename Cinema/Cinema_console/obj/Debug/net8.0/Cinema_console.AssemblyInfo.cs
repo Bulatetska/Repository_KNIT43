b22318a18bc673abeffc1ab7aef9f827451cadc1
viktorkhrystenko1 <viktor.khrystenko1@gmail.com>
2026-09-23 14:57:57 +0300

    Lab 2 done

--- Cinema/Cinema_console/obj/Debug/net8.0/Cinema_console.AssemblyInfo.cs
+++ Cinema/Cinema_console/obj/Debug/net8.0/Cinema_console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cinema_console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c5769a01c82b979cd9712bc73c9f795a117af6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a60b8fbb2b995e8efe6dab87e2405255e5d5462")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cinema_console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cinema_console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Khrystenko/lab_2/Lab_2/Lab_2/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumOfDigitsInNumber();
+        }
+
+
+        static void Mean()
+        {
+            double n1;
+            double n2;
+            Console.WriteLine("Enter numbers");
+            Console.WriteLine("Number 1: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Number 2: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Mean: " + (n1 + n2) / 2);
+        }
+
+
+        static void Task2()
+        {
+            string message = """
+                To be or not to be
+                \ Shakespeare \
+                """;
+            Console.WriteLine(message);
+        }
+
+
+        static void IsEven()
+        {
+            Console.WriteLine("Enter number to check:");
+            double number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Entered number is even: " + (number % 2 == 0));
+        }
+
+
+        static void QuantityOfDigitsInNumberAndTheirSum()
+        {
+            Console.WriteLine("Enter number to check:");
+            int number = Convert.ToInt16(Console.ReadLine());
+            char[] digits = number.ToString().ToCharArray();
+            int quantityOfDigits = digits.Length;
+            int sumOfDigits = 0;
+            foreach (char digit in digits)
+            {
+                sumOfDigits += (int) char.GetNumericValue(digit);
+            }
+            Console.WriteLine("Quantitry of digits in " + number + ": " + quantityOfDigits);
+            Console.WriteLine("Sum of digits in " + number + ": " + sumOfDigits);
+        }
+
+
+        static void ReverseNumber()
+        {
+            Console.WriteLine("Enter number to reverse:");
+            int number = Convert.ToUInt16(Console.ReadLine());
+            char[] digits = number.ToString().ToCharArray();
+            Console.Write("Reversed number: ");
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                Console.Write(digits[i]);
+            }
+        }
+
+
+        static void SumOfDigitsInNumber()
+        {
+            Console.WriteLine("Enter number to check:");
+            int number = Convert.ToInt16(Console.ReadLine());
+            char[] digits = number.ToString().ToCharArray();
+            int sumOfDigits = 0;
+            foreach (char digit in digits)
+            {
+                sumOfDigits += (int)char.GetNumericValue(digit);
+            }
+            Console.WriteLine("Sum of digits in " + number + ": " + sumOfDigits);
+        }
+    }
+}
